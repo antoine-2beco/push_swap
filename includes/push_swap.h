8349fd1a58890d6e112fad0750950eb84e9baa51
antoine-2beco <mail@antoinedebeco.com>
2024-01-09 16:16:23 +0100

    0.7

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:15:11 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/01/08 16:07:15 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/01/09 16:08:55 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+# include "../printf/ft_printf.h"
+
+typedef struct s_stack
+{
+	int				nbr;
+	int				index;
+	int				push_cost;
+	bool			above_median;
+	bool			cheapest;
+	struct s_stack	*target;
+	struct s_stack	*prev;
+	struct s_stack	*next;
+}	t_stack;
 
 #endif
