4bfa415cdf3a234c94b3c8741c3ff1c57923f683
antoine-2beco <mail@antoinedebeco.com>
2024-01-15 14:43:46 +0100

    0.16

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:27:34 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/01/15 13:24:19 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:38:06 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,16 @@ int	push_swap(int argc, char *argv[])
 	//t_stack	**stack_b;
 
 	i = 0;
-	ft_printf("argc = %i\n", argc);
+	stack_a = NULL;
 	if (argc < 2)
 		return (0);
 
 	if (!init_stack(argc, argv, &stack_a))
 		return (*(int *)error(0, "init_stack return NULL\n"));
-	ft_printf("---\n");
-	while (i++ <= 2)
+	while (stack_a)
 	{
-		ft_printf("nbr = %i, index = %i, push_cost = %i\n", (stack_a)->nbr, (stack_a)->index, (stack_a)->push_cost);
-		stack_a = (stack_a)->prev;
+		ft_printf("11 %p : nbr = %i, index = %i, push_cost = %i, prev = %p, next = %p\n", stack_a, (stack_a)->nbr, (stack_a)->index, (stack_a)->push_cost,(stack_a)->prev, (stack_a)->next);
+		stack_a = (stack_a)->next;
 	}
 	return (1);
 }
--- src/stacks_utils.c
+++ src/stacks_utils.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:49:20 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/01/15 13:26:40 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:43:00 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,33 +37,6 @@ int	free_stack(t_stack **stack)
 	return (1);
 }
 
-//static t_stack	*new_node(int nbr, int index)
-//{
-//	t_stack	*new_node;
-//
-//	new_node = malloc(sizeof(t_stack));
-//	if (!new_node)
-//		return (error(0, "node is NULL in new_nde\n"));
-//	new_node->nbr = nbr;
-//	new_node->index = index;
-//	new_node->push_cost = 0;
-//	new_node->above_median = NULL;
-//	new_node->cheapest = NULL;
-//	new_node->target = NULL;
-//	new_node->prev = NULL;
-//	new_node->next = NULL;
-//	return (new_node);
-//}
-
-static t_stack	*get_last_node(t_stack *stack)
-{
-	if (!stack)
-		return (NULL);
-	while (stack->next)
-		stack = stack->next;
-	return (stack);
-}
-
 static int	add_node(t_stack **stack, int nbr, int index)
 {
 	t_stack	*node;
@@ -76,14 +49,15 @@ static int	add_node(t_stack **stack, int nbr, int index)
 		return (*(int *)error(0, "node malloc failed in add_node\n"));
 	node->nbr = nbr;
 	node->index = index;
+	node->next = NULL;
+	node->prev = NULL;
 	if (!(*stack))
-	{
 		*stack = node;
-		node->prev = NULL;
-	}
 	else
 	{
-		last_node = get_last_node(*stack);
+		last_node = *stack;
+		while (last_node->next)
+			last_node = last_node->next;
 		last_node->next = node;
 		node->prev = last_node;
 	}
@@ -105,7 +79,6 @@ int	init_stack(int argc, char *argv[], t_stack **stack)
 	}
 	while (args[++i])
 	{
-		ft_printf("arg[%i]\n", i);
 		if (!add_node(stack, ft_atoi(args[i]), i))
 			return (*(int *)error(0, "add_node return NULL\n"));
 	}
@@ -113,6 +86,3 @@ int	init_stack(int argc, char *argv[], t_stack **stack)
 		free(args);
 	return (1);
 }
-
-
-//ft_printf("11 %p : nbr = %i, index = %i, push_cost = %i, prev = %p, next = %p\n", *stack, (*stack)->nbr, (*stack)->index, (*stack)->push_cost,(*stack)->prev, (*stack)->next);
